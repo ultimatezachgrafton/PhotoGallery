4998b8eb1f0658c00f4619d3402e338a046423e7
Zach Grafton <zachgrafton@gmail.com>
2019-10-28 19:08:48 -0700

    json

--- app/src/main/java/learningcurve/zachg/photogallery/FlickrFetcher.java
+++ app/src/main/java/learningcurve/zachg/photogallery/FlickrFetcher.java
@@ -1,5 +1,8 @@
 package learningcurve.zachg.photogallery;
 
+import android.net.Uri;
+import android.util.Log;
+
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -8,6 +11,9 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class FlickrFetcher {
+    private static final String TAG = "FlickrFetchr";
+    private static final String API_KEY = "50b4240e597dc7b63dae051b368475f4";
+
     public byte[] getUrlBytes(String urlSpec) throws IOException {
         URL url = new URL(urlSpec);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
@@ -33,5 +39,21 @@ public class FlickrFetcher {
     }
     public String getUrlString(String urlSpec) throws IOException {
         return new String(getUrlBytes(urlSpec));
+    }
+
+    public void fetchItems() {
+        try {
+            String url = Uri.parse("https://api.flickr.com/services/rest/").buildUpon()
+                    .appendQueryParameter("method", "flickr.photos.getRecent")
+                    .appendQueryParameter("api_key", API_KEY)
+                    .appendQueryParameter("format", "json")
+                    .appendQueryParameter("nojsncallback", "1")
+                    .appendQueryParameter("extras", "url_s")
+                    .build().toString();
+            String jsonString = getUrlString(url);
+            Log.i(TAG, "Received JSON: " + jsonString);
+        } catch (IOException ioe) {
+            Log.e(TAG, "Failed to fetch items", ioe);
+        }
     }
 }
--- app/src/main/java/learningcurve/zachg/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/learningcurve/zachg/photogallery/PhotoGalleryFragment.java
@@ -40,12 +40,7 @@ public class PhotoGalleryFragment extends Fragment {
     private class FetchItemsTask extends AsyncTask<Void, Void, Void> {
         @Override
         protected Void doInBackground(Void... params) {
-            try {
-                String result = new FlickrFetcher().getUrlString("https://www.bignerdranch.com");
-                Log.i(TAG, "Fetched contents of URL: " + result);
-            } catch (IOException ioe) {
-                Log.e(TAG, "Failed to fetch URL: ", ioe);
-            }
+            new FlickrFetcher().fetchItems();
             return null;
         }
     }
